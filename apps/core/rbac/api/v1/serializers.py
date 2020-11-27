c1879cd8e9cfd24749f45b0a8662781f34247612
GMNaim <gmnaim3336@gmail.com>
2020-11-27 08:55:38 +0600

    Renamed CustomerSerializer to ClientSerializer

--- apps/core/rbac/api/v1/serializers.py
+++ apps/core/rbac/api/v1/serializers.py
@@ -1,34 +1,34 @@
 from rest_framework import serializers
 from apps.core.rbac.models import *
-        
-        
-class CustomerSerializer(serializers.ModelSerializer):
+
+
+class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
-        
+
     def validate_schema_name(self, value):
         """ Check that schema_name is in lower case. """
         if value not in value.lower():
             raise serializers.ValidationError("Schema Name must be in lower case")
         return value
-        
-        
+
+
 class DomainSerializer(serializers.ModelSerializer):
     class Meta:
         model = Domain
         fields = '__all__'
-        
-        
+
+
 class TenantSerializer(serializers.ModelSerializer):
-    customer = CustomerSerializer()
-    
+    customer = ClientSerializer()
+
     class Meta:
         model = Domain
         fields = '__all__'
-        
+
     def create(self, validated_data):
-        """ Create and return a new Tenant. """     
+        """ Create and return a new Tenant. """
         customer = Customer.objects.create(**validated_data.get('customer'))
         domain = Domain()
         domain.domain = validated_data.get('domain')
@@ -38,8 +38,8 @@ class TenantSerializer(serializers.ModelSerializer):
         domain.is_active = validated_data.get('is_active')
         domain.save()
         return domain
-        
-        
+
+
 class FeatureSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feature
@@ -56,41 +56,41 @@ class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
         fields = '__all__'
-        
-        
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
         datatables_always_serialize = ('id')
         extra_kwargs = {'password': {'write_only': True}}
-        
+
     def create(self, validated_data):
         """ Create and return a new User. """
         user = User.objects.create(
-            first_name = validated_data.get('first_name'),
-            middle_name = validated_data.get('middle_name'),
-            last_name = validated_data.get('last_name'),
-            position = validated_data.get('position'),
-            email = validated_data.get('email'),
-            username = validated_data.get('username'),
-            role = validated_data.get('role'),
-            mobile_number = validated_data.get('mobile_number'),
-            gender = validated_data.get('gender'),
-            address = validated_data.get('address'),
-            is_active = int(self.context['request'].data.get('account_status', 0)),
-            country = validated_data.get('country'),       
+            first_name=validated_data.get('first_name'),
+            middle_name=validated_data.get('middle_name'),
+            last_name=validated_data.get('last_name'),
+            position=validated_data.get('position'),
+            email=validated_data.get('email'),
+            username=validated_data.get('username'),
+            role=validated_data.get('role'),
+            mobile_number=validated_data.get('mobile_number'),
+            gender=validated_data.get('gender'),
+            address=validated_data.get('address'),
+            is_active=int(self.context['request'].data.get('account_status', 0)),
+            country=validated_data.get('country'),
         )
         if self.context['request'].data.get('file_profile_picture') is not None:
             user.profile_picture = self.context['request'].data['file_profile_picture']
         if self.context['request'].data.get('file_signature') is not None:
-            user.signature = self.context['request'].data['file_signature'] 
+            user.signature = self.context['request'].data['file_signature']
         user.set_password(validated_data.get('password'))
         user.save()
         return user
-        
+
     def update(self, instance, validated_data):
-        """ Update and return an existing User instance. """ 
+        """ Update and return an existing User instance. """
         instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.middle_name = validated_data.get('middle_name', instance.middle_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
@@ -124,13 +124,13 @@ class GroupSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class BranchSerializer(serializers.HyperlinkedModelSerializer):
+class BranchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Branch
         fields = '__all__'
 
 
-class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
+class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
         fields = '__all__'
