3cfdc6027c475831f9cdbcaa0815217022881bd8
Md. Ashikun Nabi <ashikunnabituhin@gmail.com>
2021-01-15 10:58:16 +0600

    INVENTORY/CATEGORY: 'Jstree plugins added'

--- apps/inventory/static/inventory/js/category.js
+++ apps/inventory/static/inventory/js/category.js
@@ -10,11 +10,13 @@ class Category {
     // fill jstree div
     set_category_in_jstree_div() {
         let self = this;
-        console.log('inside set_category_in_jstree_div()');
         $('#jstree_div').jstree({
             'core': {
+                "animation": 0,
+                "check_callback": true,
+                "themes": {"stripes": true},
                 'data': {
-                    'headers': { 'Authorization': 'JWT '+self._helper.storage.getStorage('local', 'token').access },
+                    'headers': {'Authorization': 'JWT ' + self._helper.storage.getStorage('local', 'token').access},
                     'url': function (node) {
                         return node.id === '#' ?
                             self.category_list_url :
@@ -24,7 +26,30 @@ class Category {
                         return {'id': node.id};
                     }
                 }
-            }
+            },
+            "types": {
+                "#": {
+                    "max_children": -1,
+                    "max_depth": -1,
+                    "valid_children": ["root"]
+                },
+                "root": {
+                    "icon": "fas fa-large fa-folder",
+                    "valid_children": ["default"]
+                },
+                "default": {
+                    "icon": "fas fa-large fa-folder",
+                    "valid_children": ["default", "file"]
+                },
+                "file": {
+                    "icon": "fas fa-large fa-folder",
+                    "valid_children": []
+                }
+            },
+            "plugins": [
+                "contextmenu", "dnd", "search",
+                "state", "types", "wholerow"
+            ]
         });
     }
 }
